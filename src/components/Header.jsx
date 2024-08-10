1f0027e45f4a6cbbd9fd7921877b93e6da90cc30
SATYAM KUMAR <workmail.celestial@gmail.com>
2024-08-10 21:14:56 +0530

    Routing
    React routing is done

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+
+
+    <nav>
+      <div className="container nav__container">
+        <link to="/" className='nav__logo' >
+          <img src="" alt="" />
+        </link>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
