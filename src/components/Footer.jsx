0bf6353113718d0e1abe91b3fae30ca18342d814
SATYAM KUMAR <workmail.celestial@gmail.com>
2024-08-11 21:14:25 +0530

    error page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,22 +3,23 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <Footer >
-      <ul className="footer__categories">
-        <li><Link to="posts/categories/:Agriculture">Agriculture</Link></li>
-        <li><Link to="posts/categories/:Business">Business</Link></li>
-        <li><Link to="posts/categories/:Education">Education</Link></li>
-        <li><Link to="posts/categories/:Entertainment">Entertainment</Link></li>
-        <li><Link to="posts/categories/:Art">Art</Link></li>
-        <li><Link to="posts/categories/:Investment">Investment</Link></li>
-        <li><Link to="posts/categories/:Uncategorized">Uncategorized</Link></li>
-        <li><Link to="posts/categories/:Weather">Weather</Link></li>
-      </ul>
+    <footer>
+    <ul className="footer__categories">
 
-      <div className="footer__copyright">
-        <small>All Rights Reserved &copy; Copyright, Celestial</small>
-      </div>
-    </Footer> 
+      <li><Link to="/posts/categories/Agriculture">Agriculture</Link></li>
+      <li><Link to="/posts/categories/Business">Business</Link></li>
+      <li><Link to="/posts/categories/Education">Education</Link></li>
+      <li><Link to="/posts/categories/Entertainment">Entertainment</Link></li>
+      <li><Link to="/posts/categories/Art">Art</Link></li>
+      <li><Link to="/posts/categories/Investment">Investment</Link></li>
+      <li><Link to="/posts/categories/Uncategorized">Uncategorized</Link></li>
+      <li><Link to="/posts/categories/Weather">Weather</Link></li>
+    </ul>
+    <div className="footer__copyright">
+      <small>All Rights Reserved &copy; Copyright, celestial.</small>
+    </div>
+    
+  </footer>
   )
 }
 
