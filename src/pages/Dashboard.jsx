7dfb17f68dd2f93dcccd4461b94ab07ca9b7081e
SATYAM KUMAR <workmail.celestial@gmail.com>
2024-08-13 00:04:01 +0530

    posts

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {DUMMY_POSTS} from '../data'
 
 const Dashboard = () => {
+
+  const [posts, setPosts] = useState(DUMMY_POSTS)
+
   return (
-    <div>Dashboard</div>
+    <section className="dashboard">
+      {
+        posts.length ? <div className="container dashboard__container">
+        {
+          posts.map(post => {
+            return <article key={post.id} className='dashboard__post' >
+              <div className="dashboard__post-info">
+                <div className="dashboard__post-thumbnail">
+                  
+                </div>
+              </div>
+              <div className="dashboard__post-actions"></div>
+            </article>
+        })
+        }
+        </div> : <h2 className="center">You have no posts yet.</h2>
+      }
+    </section>
   )
 }
 
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Avatar from '../images/avatar15.jpg'
+import { FaEdit } from 'react-icons/fa'
+import { FaCheck } from 'react-icons/fa'
 
 const UserProfile = () => {
+
+  const [avatar, setAvatar] = useState(Avatar)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [currentPassword, setcurrentPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [confirmNewPassword, setConfirmNewPassword] = useState('')
+
   return (
-    <div>UserProfile</div>
+    <section className="profile">
+      <div className="container profile__container">
+        <Link to={`/myposts/sdfsdf`} classname='btn' >My posts</Link>
+        <div className="profile__details">
+          <div className="avatar__wrapper">
+           <div className="profile__avatar">
+            <img src={avatar} alt="" />
+            </div> 
+            <form  className="avatar__form">
+              <input type="file" name='avatar' onChange={e => setAvatar(e.target.files[0])} id='avatar' accept='png, jpg, jpeg' />
+              <label htmlFor="avatar"><FaEdit /></label> 
+            </form>
+            <button className="profile__avatar-btn"><FaCheck /></button>
+          </div>
+          <h1>Celestial</h1>
+          <form  className="form profile__form">
+            <p className="form__error-message">
+              This is an error message.</p>
+            <input type="text" placeholder='Full Name' value={name} onChange={e => setName(e.target.value)} />
+            <input type="email" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} />
+            <input type="password" placeholder='Current Password' value={currentPassword} onChange={e => setcurrentPassword(e.target.value)} />
+            <input type="password" placeholder='New Password' value={newPassword} onChange={e => setNewPassword(e.target.value)} />
+            <input type="password" placeholder='Confirm New Password' value={confirmNewPassword} onChange={e => setConfirmNewPassword(e.target.value)} />
+            <button type="submit" className='btn primary'>Update Details</button>
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
